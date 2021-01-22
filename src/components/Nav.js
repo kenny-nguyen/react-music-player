b9f1b718fcf74220ac9911015c5fe4d9caa0de15
Kenny <kennyndev@gmail.com>
2021-01-22 17:47:40 -0500

    added darkmode-toggle, styling needs to be done still

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,21 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
+// darkmode-toggle tutorial
+// https://css-tricks.com/a-dark-mode-toggle-with-react-and-themeprovider/
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../darkmode-toggle/theme";
+import { GlobalStyles } from "../darkmode-toggle/global";
+import Toggle from "../darkmode-toggle/Toggle";
+import { useDarkMode } from "../darkmode-toggle/useDarkMode";
+
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />;
+  }
   return (
     <nav>
       <h1>Tunes</h1>
@@ -10,6 +24,12 @@ const Nav = ({ libraryStatus, setLibraryStatus }) => {
         Library
         <FontAwesomeIcon icon={faMusic} />
       </button>
+      <ThemeProvider theme={themeMode}>
+        <>
+          <GlobalStyles />
+          <Toggle theme={theme} toggleTheme={toggleTheme} />
+        </>
+      </ThemeProvider>
     </nav>
   );
 };
